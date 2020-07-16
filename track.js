17b44102ec6472116aa4637cab63cf32c2f298b7
rasturize <connor@rasturize.com>
2020-07-16 11:59:23 -0400

    test

--- track.js
+++ track.js
@@ -4,29 +4,41 @@ function triggerAlerts() {
   let menu = document.getElementById("detect");
   let alert = document.createElement("li");
 
-  if (Modernizr.canvas) {
-    alert.textContent = "Canvas enabled";
-    menu.appendChild(alert);
-    break;
+  switch() {
+    default:
+      text = "results will be shown here..."
+      break;
+    case 0:
+      if (Modernizr.canvas) {
+        alert.textContent = "Canvas enabled";
+        menu.appendChild(alert);
+        break;
+      }
   }
 
-  if (Modernizr.cookies) {
-    alert.textContent = "Cookies enabled";
-    menu.appendChild(alert);
-    break;
-  }
-
-  if (Modernizr.cors) {
-    alert.textContent = "Cross-Domain sharing enabled";
-    menu.appendChild(alert);
-    break;
-  }
-
-  if (Modernizr.fullscreen) {
-    alert.textContent = "Fullscreen takeover enabled";
-    menu.appendChild(alert);
-    break;
-  }
+  // if (Modernizr.canvas) {
+  //   alert.textContent = "Canvas enabled";
+  //   menu.appendChild(alert);
+  //   break;
+  // }
+  //
+  // if (Modernizr.cookies) {
+  //   alert.textContent = "Cookies enabled";
+  //   menu.appendChild(alert);
+  //   break;
+  // }
+  //
+  // if (Modernizr.cors) {
+  //   alert.textContent = "Cross-Domain sharing enabled";
+  //   menu.appendChild(alert);
+  //   break;
+  // }
+  //
+  // if (Modernizr.fullscreen) {
+  //   alert.textContent = "Fullscreen takeover enabled";
+  //   menu.appendChild(alert);
+  //   break;
+  // }
 
 }
 
