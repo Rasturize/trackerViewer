c22fe6b19e9d370b46df79a09a1a0f5d8aab104a
rasturize <connor@rasturize.com>
2020-07-16 11:42:11 -0400

    small cleanup

--- track.js
+++ track.js
@@ -1,26 +1,46 @@
 
 
+let menu = document.getElementById("detect");
+let alert = document.createElement("li");
 
-
-
-
-
-if (Modernizr.geolocation) {
-
-  navigator.geolocation.getCurrentPosition(function(position) {
-
-    let latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-    let myOptions = {
-      zoom: 8,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.TERRAIN,
-      disableDefaultUI: true
-    }
-    let map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-  });
-
-} else {
-  const para = document.createElement('p');
-  para.textContent = 'Argh, no geolocation!';
-  document.body.appendChild(para);
+if (Modernizr.canvas) {
+  alert.textContent = "Canvas enabled";
+  menu.appendChild(alert);
 }
+
+if (Modernizr.cookies) {
+  alert.textContent = "Cookies enabled";
+  menu.appendChild(alert);
+}
+
+if (Modernizr.cors) {
+  alert.textContent = "Cross-Domain sharing enabled";
+  menu.appendChild(alert);
+}
+
+if (Modernizr.fullscreen) {
+  alert.textContent = "Fullscreen takeover enabled";
+  menu.appendChild(alert);
+}
+
+
+ // ctrl + /
+// if (Modernizr.geolocation) {
+//
+//   navigator.geolocation.getCurrentPosition(function(position) {
+//
+//     let latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+//     let myOptions = {
+//       zoom: 8,
+//       center: latlng,
+//       mapTypeId: google.maps.MapTypeId.TERRAIN,
+//       disableDefaultUI: true
+//     }
+//     let map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+//   });
+//
+// } else {
+//   const para = document.createElement('p');
+//   para.textContent = 'no geolocation!';
+//   document.body.appendChild(para);
+// }
